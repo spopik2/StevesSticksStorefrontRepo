1aba73123993f905b0ba52b4f439ab5a6ebd9af4
spopik2 <70964549+spopik2@users.noreply.github.com>
2022-04-14 15:08:03 -0600

    storefront finished

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,6 @@ export default function Home(props) {
 
   return (
     <>
-    <Head>
-      <meta charset="UTF-8"/>
-      <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <title>Storefront</title>
-    </Head>
     <PageTitle title="StoreFront" tagline="featured products"/> 
     <main>
         { products.map(product => <ProductCard key={product.uid} product={product} />)}
