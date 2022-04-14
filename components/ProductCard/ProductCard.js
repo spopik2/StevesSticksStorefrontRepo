d4e26dd4e96de8a0eab7d3e902206788de1de731
spopik2 <70964549+spopik2@users.noreply.github.com>
2022-04-14 14:57:44 -0600

    storefront finished

---
+++ components/ProductCard/ProductCard.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Image from 'next/image'
+import {productCard, price, name, description} from './styles.module.scss'
+
+function ProductCard ({children, product, ...props})  {
+  const {productName, productPrice, productDescription, imageUrl}= {...product}
+  return (
+       <aside className={productCard}>
+         <header>
+            <Image
+              src={imageUrl}
+              alt={productName}
+              width={418}
+              height={240}
+              quality={50}
+            />
+          
+         </header>
+          <p className={name}>{productName}</p>
+          <p className={price}>${productPrice}</p>
+          <p className={description}>{productDescription}</p>
+          <footer>
+            <form action="#" method="POST">
+              <input type="hidden" name="uid" value="uid"/>
+              <button type="submit">Buy Now</button>
+            </form>
+          </footer>
+       </aside>
+  )
+}
+
+export default ProductCard
